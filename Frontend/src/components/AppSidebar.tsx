a0ada6f497f0de8e8d8dffc2d0b7e6a1c14da94a
RubabMubashar <rubabmubashar174@gmail.com>
2025-08-22 18:25:33 +0500

    Adding Auth System

--- Frontend/src/components/AppSidebar.tsx
+++ Frontend/src/components/AppSidebar.tsx
@@ -1,5 +1,5 @@
-import { NavLink, useLocation } from "react-router-dom";
-import { BarChart3, Home } from "lucide-react";
+import { NavLink, useLocation , useNavigate } from "react-router-dom";
+import { BarChart3, Home , LogOut } from "lucide-react";
 import { portfolios } from "@/data/PortfolioList";
 
 import {
@@ -18,10 +18,16 @@ import {
 export function AppSidebar() {
   const { state } = useSidebar();
   const location = useLocation();
+  const navigate = useNavigate();
   const currentPath = location.pathname;
   const collapsed = state === "collapsed";
 
   const isActive = (path: string) => currentPath === path;
+
+   const handleLogout = () => {
+    localStorage.removeItem("isAuth")
+    navigate("/") // back to login page
+  }
 
   return (
     <Sidebar className={collapsed ? "w-14" : "w-64"} collapsible="icon">
@@ -47,12 +53,12 @@ export function AppSidebar() {
                 <SidebarMenuButton
                   asChild
                   className={
-                    isActive("/")
+                    isActive("/home")
                       ? "bg-sidebar-accent text-sidebar-primary font-medium"
                       : "hover:bg-sidebar-accent/50"
                   }
                 >
-                  <NavLink to="/" className="flex items-center space-x-2">
+                  <NavLink to="/home" className="flex items-center space-x-2">
                     <Home className="h-4 w-4" />
                     {!collapsed && <span>Total Management</span>}
                   </NavLink>
@@ -90,6 +96,17 @@ export function AppSidebar() {
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
+
+        {/* Logout Button at bottom */}
+        <div className="mt-auto p-4">
+          <button
+            onClick={handleLogout}
+            className={`${collapsed ? "bg-transparent text-green-500 hover:bg-green-600/20" : "bg-green-600 text-white hover:bg-green-700"} w-full flex items-center justify-center space-x-2 rounded-md py-2  text-sm font-semibold transition`}
+          >
+            <LogOut className="h-4 w-4" />
+            {!collapsed && <span>Logout</span>}
+          </button>
+        </div>
       </SidebarContent>
     </Sidebar>
   );
